8434eb73aabd53b5b74c8085b1e28a7e893eea65
Sanam Udash <sanamudash@Sanams-MacBook-Pro.local>
2023-06-23 08:56:33 +0545

    first

---
+++ coffeshop/src/test/java/com/project/coffeshop/CoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.coffeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
